ba404e5acf856d3495ac70590d86d6184cd5ac54
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-08-30 23:11:40 -0500

    data base connect

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,26 @@
-console.log("hello")
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app=express()
+app.use(express.json())
+
+
+mongoose.connect(process.env.mongoose_url)
+.then(
+    console.log("db connect"),
+app.listen(process.env.PORT,()=>{
+console.log(`App running at the port ${process.env.PORT}`)
+
+app.get('/',(req,res)=>{
+res.send("Api is running")
+})
+})
+
+
+)
+.catch((err) => {
+  console.log("DB connection failed:", err);
+});
