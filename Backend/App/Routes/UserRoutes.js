185be2b25d26692c00e59eea7903c6b35e917d39
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-09-02 12:01:29 -0500

    logout routes done

--- Backend/App/Routes/UserRoutes.js
+++ Backend/App/Routes/UserRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import {register,login,Verification} from '../Controllers/Usercontroller.js'
+import {register,login,Verification,Logout} from '../Controllers/Usercontroller.js'
+import auth from '../Middleware/Auth.js'
 const userRoutes=express.Router()
 
 userRoutes.post('/register',register)
 userRoutes.post('/login',login)
 userRoutes.post('/verify',Verification)
+userRoutes.post('/logout',auth,Logout)
+
 export default userRoutes
