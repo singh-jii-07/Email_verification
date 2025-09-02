f082e6ab7cdfdd94fe34264cb14ff60a09fadb3f
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-09-02 12:53:05 -0500

    otp routes done

--- Backend/App/Routes/UserRoutes.js
+++ Backend/App/Routes/UserRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {register,login,Verification,Logout,forgotPassword} from '../Controllers/Usercontroller.js'
+import {register,login,Verification,Logout,forgotPassword,verifyOtp} from '../Controllers/Usercontroller.js'
 import auth from '../Middleware/Auth.js'
 const userRoutes=express.Router()
 
@@ -7,6 +7,7 @@ userRoutes.post('/register',register)
 userRoutes.post('/login',login)
 userRoutes.post('/verify',Verification)
 userRoutes.post('/forgot',forgotPassword)
+userRoutes.post('/otp',verifyOtp)
 userRoutes.post('/logout',auth,Logout)
 
 export default userRoutes
