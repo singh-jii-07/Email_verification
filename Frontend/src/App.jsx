208bd072288bdc0d4fc4729a820d35b00af508ac
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-09-03 23:04:46 -0500

    frontend folder done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-  <div>
-    hello
-  </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
